e377a04c5111eb421209d3283eae46c70a9ae65e
Fadora <fabiandora26@gmail.com>
2023-05-08 02:33:13 +0200

    adding ORM layer

--- app/src/main/java/com/mobillabor/moblab/model/Character.kt
+++ app/src/main/java/com/mobillabor/moblab/model/Character.kt
@@ -1,5 +1,23 @@
 package com.mobillabor.moblab.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "character")
 data class Character(
-    val name: String
+    @PrimaryKey(autoGenerate = false) var charId: Long,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "gender") val gender: String,
+    @ColumnInfo(name = "realm") val realm: String,
+    @ColumnInfo(name = "race") val race: String,
+    @ColumnInfo(name = "birthdate") val birthdate: String,
+    @ColumnInfo(name = "spouse") val spouse: String,
+    @ColumnInfo(name = "death") val dateofdeath: String,
+    @ColumnInfo(name = "height") val height: String,
+    @ColumnInfo(name = "hair") val hair: String,
+    @ColumnInfo(name = "quotes") val quotes: List<String>,
+
+
 )
